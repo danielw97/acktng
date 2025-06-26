e9bf50b27e9e05bd1f28799526df5ebf412a2374
Ackmud Historical Archive <ackmud@ackmud.com>
2025-06-26 19:40:33 +0200

    Weapons grade derp

--- src/act_info.c
+++ src/act_info.c
@@ -2150,20 +2150,20 @@ void do_who(CHAR_DATA *ch, char *argument)
       }
       for (d = first_desc; d != NULL; d = d->next)
       {
-         if (i == 0 && ch->level <= MAX_MORTAL)
+         if (i == 0 && wch->level <= MAX_MORTAL)
             continue;
-         if (i == 1 && (!is_adept(ch) || ch->level > MAX_MORTAL))
+         if (i == 1 && (!is_adept(wch) || wch->level > MAX_MORTAL))
             continue;
-         if (i == 2 && (!is_remort(ch) || is_adept(ch) || ch->level > MAX_MORTAL))
+         if (i == 2 && (!is_remort(wch) || is_adept(wch) || wch->level > MAX_MORTAL))
             continue;
-         if (i == 3 && (is_remort(ch) || is_adept(ch) || ch->level > MAX_MORTAL))
+         if (i == 3 && (is_remort(wch) || is_adept(wch) || wch->level > MAX_MORTAL))
             continue;
          CHAR_DATA *wch = (d->original != NULL) ? d->original : d->character;
          char const *class;
 
          class = class_table[wch->class].who_name;
 
-         if(ch->level > MAX_MORTAL)
+         if(wch->level > MAX_MORTAL)
          {
             switch (wch->level)
             {
