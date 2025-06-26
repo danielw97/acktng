1a116bf5f5ee7d301aa4ecbbd00dc0b093be1521
Ackmud Historical Archive <ackmud@ackmud.com>
2025-06-26 18:14:51 +0200

    Oops, gotta fix remort levs

--- src/macros.c
+++ src/macros.c
@@ -45,7 +45,7 @@ void reset_gain_stats( CHAR_DATA * ch )
    for(int i = 0; i < MAX_CLASS; i++)
    {
       curr = ch->lvl[i];
-      ch->lvl[i] = 0;
+      ch->lvl[i] = -1;
       for(int j = 0; j < curr; j++)
       {
          advance_level(ch, i, FALSE);
@@ -55,7 +55,7 @@ void reset_gain_stats( CHAR_DATA * ch )
    for(int i = 0; i < MAX_REMORT; i++)
    {
       curr = ch->remort[i];
-      ch->remort[i] = 0;
+      ch->remort[i] = -1;
       for(int j = 0; j < curr; j++)
       {
          advance_level_remort(ch, i, FALSE);
@@ -65,7 +65,7 @@ void reset_gain_stats( CHAR_DATA * ch )
    for(int i = 0; i < MAX_CLASS; i++)
    {
       curr = ch->adept[i];
-      ch->adept[i] = 0;
+      ch->adept[i] = -1;
       for(int j = 0; j < curr; j++)
       {
          advance_level_adept(ch, i, FALSE);
