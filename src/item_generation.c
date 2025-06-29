bda9a4b985d64371e09310905c859589293147a9
Ackmud Historical Archive <ackmud@ackmud.com>
2025-06-29 15:46:03 +0200

    Adding declaration for suffixes

--- src/item_generation.c
+++ src/item_generation.c
@@ -3,6 +3,7 @@
 char *get_wear_name(OBJ_DATA *obj);
 void set_obj_stat_auto(OBJ_DATA *obj);
 void set_aff_to_obj(OBJ_DATA *obj, int location, int modifier);
+char *get_suffix(OBJ_DATA *obj);
 
 OBJ_DATA *generate_item(int level)
 {
